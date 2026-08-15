b648f597f44e98cf26217e1793fc164834ae9820
Prince-ABES <prk988110@gmail.com>
2026-08-15 17:11:12 +0530

    Todo- functionality part done

--- 10todocontextlocal/src/App.jsx
+++ 10todocontextlocal/src/App.jsx
@@ -6,6 +6,26 @@ import './App.css'
 import {TodoProvider} from "./contexts"
 
 function App() {
+  const [todos, setTodos] =useState([]);
+
+  const addTodo = (todo) =>{
+    setTodos((prev) =>[ {id: Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) =>{
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete =(id) => {
+    setTodos((prev) => 
+      prev.map((prevTodo) => 
+      prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : {prevTodo})
+    )
+  }
   
 
   return (
